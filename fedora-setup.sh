9b01dfbe0f5075db6c98a98b715135b1938aca8d
Chris Titus <dfm.titus@gmail.com>
2018-11-05 11:25:40 -0600

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/bash
 
 set -e
+# Set Virtual Box repo
+sudo wget http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo -P /etc/yum.repos.d/
+# Install Rpmfusion repo
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
-dnf install $(cat fedora.packages) -y
+# grab all packages to install
+sudo dnf install $(cat fedora.packages) -y
 
 git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 cp spacemacs.el ~/.spacemacs
 
 cat bashrc.aliases >> ~/.bashrc
 
-echo "TODO: Gnome Tweak: Alternate Tab"
-echo "TODO: Gnome Tweak: Open New Window Instance"
-echo "TODO: Download Chrome"
-echo "TODO: Install Chrome"
-echo "TODO: dnf update -y"
+sudo dnf upgrade -y
