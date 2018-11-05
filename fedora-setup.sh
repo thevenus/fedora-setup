21163b261457e13c4c5c34b74726cb80b3de56d9
Chris Titus <dfm.titus@gmail.com>
2018-11-05 12:09:39 -0600

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/bash
 
 set -e
+# First we setup the repositories so we can install lots of stuff
 # Set Virtual Box repo
 sudo wget http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo -P /etc/yum.repos.d/
+# Install Google Chrome repo
+sudo dnf install fedora-workstation-repositories -y
 # Install Rpmfusion repo
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-# Install Google Chrome repo
-sudo cat << EOF > /etc/yum.repos.d/google-chrome.repo
-[google-chrome]
-name=google-chrome
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=1
-gpgkey=https://dl.google.com/linux/linux_signing_key.pub
-EOF
 # Update cache for package installs
 sudo dnf makecache
+
 # grab all packages to install
 sudo dnf install $(cat fedora.packages) -y
 
